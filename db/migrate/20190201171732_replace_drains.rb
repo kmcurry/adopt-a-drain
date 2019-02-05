bd252c3d7571bb5a39602c155c60f7540e24001e
Bret Fisher <bret@bretfisher.com>
2019-02-05 16:21:15 -0500

    remove last migration (not needed)

--- db/migrate/20190201171732_replace_drains.rb
+++
@@ -1,8 +0,0 @@
-class ReplaceDrains < ActiveRecord::Migration
-  require 'csv'
-  def change
-    CSV.foreach('./norfolk.csv', :headers => true) do |row|
-      Thing.create!(row.to_hash)
-    end
-  end
-end
